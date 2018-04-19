84a2b9b8e236f0d721d616bce989a667b96871c3
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-04-19 22:44:31 +0100

    Add software

---
+++ software/default/dep/DisplayValue.o.d
@@ -0,0 +1,83 @@
+DisplayValue.o: ../DisplayValue.c /usr/avr/include/stdlib.h \
+ /usr/lib/gcc/avr/7.2.0/include/stddef.h /usr/avr/include/string.h \
+ ../Transistortester.h /usr/avr/include/avr/io.h \
+ /usr/avr/include/avr/sfr_defs.h /usr/avr/include/inttypes.h \
+ /usr/lib/gcc/avr/7.2.0/include/stdint.h /usr/avr/include/stdint.h \
+ /usr/avr/include/avr/iom168.h /usr/avr/include/avr/iomx8.h \
+ /usr/avr/include/avr/portpins.h /usr/avr/include/avr/common.h \
+ /usr/avr/include/avr/version.h /usr/avr/include/avr/fuse.h \
+ /usr/avr/include/avr/lock.h /usr/avr/include/util/delay.h \
+ /usr/avr/include/util/delay_basic.h /usr/avr/include/math.h \
+ /usr/avr/include/avr/sleep.h /usr/avr/include/avr/eeprom.h \
+ /usr/avr/include/avr/pgmspace.h /usr/avr/include/avr/wdt.h \
+ /usr/avr/include/avr/interrupt.h ../config.h ../part_defs.h \
+ ../lcd_defines.h ../font.h ../autoconf.h ../tt_function.h \
+ ../wait1000ms.h ../lcd-routines.h ../tt_resistor.h ../tt_globals.h
+
+/usr/avr/include/stdlib.h:
+
+/usr/lib/gcc/avr/7.2.0/include/stddef.h:
+
+/usr/avr/include/string.h:
+
+../Transistortester.h:
+
+/usr/avr/include/avr/io.h:
+
+/usr/avr/include/avr/sfr_defs.h:
+
+/usr/avr/include/inttypes.h:
+
+/usr/lib/gcc/avr/7.2.0/include/stdint.h:
+
+/usr/avr/include/stdint.h:
+
+/usr/avr/include/avr/iom168.h:
+
+/usr/avr/include/avr/iomx8.h:
+
+/usr/avr/include/avr/portpins.h:
+
+/usr/avr/include/avr/common.h:
+
+/usr/avr/include/avr/version.h:
+
+/usr/avr/include/avr/fuse.h:
+
+/usr/avr/include/avr/lock.h:
+
+/usr/avr/include/util/delay.h:
+
+/usr/avr/include/util/delay_basic.h:
+
+/usr/avr/include/math.h:
+
+/usr/avr/include/avr/sleep.h:
+
+/usr/avr/include/avr/eeprom.h:
+
+/usr/avr/include/avr/pgmspace.h:
+
+/usr/avr/include/avr/wdt.h:
+
+/usr/avr/include/avr/interrupt.h:
+
+../config.h:
+
+../part_defs.h:
+
+../lcd_defines.h:
+
+../font.h:
+
+../autoconf.h:
+
+../tt_function.h:
+
+../wait1000ms.h:
+
+../lcd-routines.h:
+
+../tt_resistor.h:
+
+../tt_globals.h:
---
+++ software/default/dep/main.o.d
@@ -0,0 +1,124 @@
+main.o: ../main.c /usr/avr/include/avr/io.h \
+ /usr/avr/include/avr/sfr_defs.h /usr/avr/include/inttypes.h \
+ /usr/lib/gcc/avr/7.2.0/include/stdint.h /usr/avr/include/stdint.h \
+ /usr/avr/include/avr/iom168.h /usr/avr/include/avr/iomx8.h \
+ /usr/avr/include/avr/portpins.h /usr/avr/include/avr/common.h \
+ /usr/avr/include/avr/version.h /usr/avr/include/avr/fuse.h \
+ /usr/avr/include/avr/lock.h /usr/avr/include/util/delay.h \
+ /usr/avr/include/util/delay_basic.h /usr/avr/include/math.h \
+ /usr/avr/include/avr/sleep.h /usr/avr/include/stdlib.h \
+ /usr/lib/gcc/avr/7.2.0/include/stddef.h /usr/avr/include/string.h \
+ /usr/avr/include/avr/eeprom.h /usr/avr/include/avr/pgmspace.h \
+ /usr/avr/include/avr/wdt.h ../Transistortester.h \
+ /usr/avr/include/avr/interrupt.h ../config.h ../part_defs.h \
+ ../lcd_defines.h ../font.h ../autoconf.h ../tt_function.h \
+ ../wait1000ms.h ../lcd-routines.h ../tt_resistor.h ../tt_globals.h \
+ ../langFRANCAIS.h ../langGERMAN.h ../langITALIAN.h ../langPOLISH.h \
+ ../langCZECH.h ../langSLOVAK.h ../langSLOVENE.h ../langDUTCH.h \
+ ../langBRASIL.h ../langSPANISH.h ../langRUSSIAN.h ../langUKRAINIAN.h \
+ ../langHUNGARIAN.h ../langLITHUANIAN.h ../langDANISH.h ../AutoCheck.c \
+ ../mark_as_uncalibrated.c ../GetIr.c
+
+/usr/avr/include/avr/io.h:
+
+/usr/avr/include/avr/sfr_defs.h:
+
+/usr/avr/include/inttypes.h:
+
+/usr/lib/gcc/avr/7.2.0/include/stdint.h:
+
+/usr/avr/include/stdint.h:
+
+/usr/avr/include/avr/iom168.h:
+
+/usr/avr/include/avr/iomx8.h:
+
+/usr/avr/include/avr/portpins.h:
+
+/usr/avr/include/avr/common.h:
+
+/usr/avr/include/avr/version.h:
+
+/usr/avr/include/avr/fuse.h:
+
+/usr/avr/include/avr/lock.h:
+
+/usr/avr/include/util/delay.h:
+
+/usr/avr/include/util/delay_basic.h:
+
+/usr/avr/include/math.h:
+
+/usr/avr/include/avr/sleep.h:
+
+/usr/avr/include/stdlib.h:
+
+/usr/lib/gcc/avr/7.2.0/include/stddef.h:
+
+/usr/avr/include/string.h:
+
+/usr/avr/include/avr/eeprom.h:
+
+/usr/avr/include/avr/pgmspace.h:
+
+/usr/avr/include/avr/wdt.h:
+
+../Transistortester.h:
+
+/usr/avr/include/avr/interrupt.h:
+
+../config.h:
+
+../part_defs.h:
+
+../lcd_defines.h:
+
+../font.h:
+
+../autoconf.h:
+
+../tt_function.h:
+
+../wait1000ms.h:
+
+../lcd-routines.h:
+
+../tt_resistor.h:
+
+../tt_globals.h:
+
+../langFRANCAIS.h:
+
+../langGERMAN.h:
+
+../langITALIAN.h:
+
+../langPOLISH.h:
+
+../langCZECH.h:
+
+../langSLOVAK.h:
+
+../langSLOVENE.h:
+
+../langDUTCH.h:
+
+../langBRASIL.h:
+
+../langSPANISH.h:
+
+../langRUSSIAN.h:
+
+../langUKRAINIAN.h:
+
+../langHUNGARIAN.h:
+
+../langLITHUANIAN.h:
+
+../langDANISH.h:
+
+../AutoCheck.c:
+
+../mark_as_uncalibrated.c:
+
+../GetIr.c:
